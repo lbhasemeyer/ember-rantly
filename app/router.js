61d2ec01ee5b3acb16cd6d44ad6138cce91efe68
lkbhas <lbhasemeyer@gmail.com>
2015-03-16 12:47:25 -0600

    params passing for registration, the rest still to come

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('signin');
+  this.route('signup');
   this.route('signout');
   this.resource('rants',  { path: '/' }, function() {
     this.route('search',  { path: '/search' });

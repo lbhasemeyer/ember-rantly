ea5276b70a841195104205fe34ec9d26658dbc3e
lkbhas <lbhasemeyer@gmail.com>
2015-03-20 11:19:43 -0600

    users who sign up get popup after user is created to sign in

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -6,39 +6,39 @@ export default Ember.ArrayController.extend({
 
     register: function() {
       var controller = this;
-      var firstName = this.get('fName');
-      var lastName  = this.get('lName');
-      var email     = this.get('userEmail').trim();
-      var password  = this.get('userPassword');
-      var passwordConfirm  = this.get('passwordConfirm');
+      var firstName = this.get('fName'),
+      lastName  = this.get('lName'),
+      email     = this.get('userEmail').trim(),
+      password  = this.get('userPassword'),
+      passwordConfirm  = this.get('passwordConfirm');
 
       var user = controller.store.createRecord('user',
     { first_name: firstName,
       last_name: lastName,
       email: email,
-      password: password,
-      password_confirmation: passwordConfirm});
+      password_digest: password});
 
       controller.set('fName', '');
       controller.set('lName', '');
       controller.set('userEmail', '');
       controller.set('userPassword', '');
-      controller.set('passwordConfirm', '');
 
-      user.save().then(function(){
-
-        var data = {
-          email: controller.get("userEmail"),
-          password: controller.get("userPassword")
-        };
-        controller.set('errorMessage', null);
-        var session = controller.store.createRecord('session', data);
-        session.save().then(function(){
-          controller.set('loggedIn', true);
-          localStorage.setItem('authToken', session._data.token);
-          controller.transitionToRoute('rants');
-        });
-      });
+      user.save();
+      alert("You have created an account.  Please sign in to continue.");
+      // .then(function(){
+      //
+      //   var data = {
+      //     email: controller.get("userEmail"),
+      //     password: controller.get("userPassword")
+      //   };
+      //   controller.set('errorMessage', null);
+      //   var session = controller.store.createRecord('session', data);
+      //   session.save().then(function(){
+      //     controller.set('loggedIn', true);
+      //     localStorage.setItem('authToken', session._data.token);
+      //     controller.transitionToRoute('rants');
+      //   });
+      // });
     }
 
   }

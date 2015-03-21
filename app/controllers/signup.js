74ad388b06f9c9bca4dfb970541bdcb2cb313ddc
lkbhas <lbhasemeyer@gmail.com>
2015-03-21 12:06:42 -0600

    user is signed in at registration

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -10,37 +10,33 @@ export default Ember.ArrayController.extend({
       var controller = this;
 
       var user = controller.store.createRecord('user',
-    { firstName: this.get('first'),
-    lastName: this.get('last'),
-    email: this.get('email'),
-    password: this.get('password'),
-    passwordConfirmation: this.get('passwordconfirmation')
-  });
+      { firstName: this.get('first'),
+      lastName: this.get('last'),
+      email: this.get('email'),
+      password: this.get('password'),
+      passwordConfirmation: this.get('passwordconfirmation')
+    });
 
-  controller.set('first_name', '');
-  controller.set('last_name', '');
-  controller.set('email', '');
-  controller.set('password', '');
-  controller.set('password_confirmation', '');
+    return user.save().then(function(){
+      var data = {
+        email: controller.get("email"),
+        password: controller.get("password")
+      };
+      controller.set('errorMessage', null);
+      var session = controller.store.createRecord('session', data);
+      session.save().then(function(){
+        controller.set('loggedIn', true);
+        localStorage.setItem('authToken', session._data.token);
+        controller.transitionToRoute('rants');
+      });
+    });
 
-  return user.save().then(function(response) {
-    alert("You have created an account.  Please sign in to continue.");
-  });
-  // .then(function(){
-  //
-  //   var data = {
-  //     email: controller.get("userEmail"),
-  //     password: controller.get("userPassword")
-  //   };
-  //   controller.set('errorMessage', null);
-  //   var session = controller.store.createRecord('session', data);
-  //   session.save().then(function(){
-  //     controller.set('loggedIn', true);
-  //     localStorage.setItem('authToken', session._data.token);
-  //     controller.transitionToRoute('rants');
-  //   });
-  // });
-}
+    controller.set('firstName', '');
+    controller.set('lastName', '');
+    controller.set('email', '');
+    controller.set('password', '');
+    controller.set('passwordConfirmation', '');
 
-}
+    }
+  }
 });
